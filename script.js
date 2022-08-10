9344b44f05d16d3be572452d06f970b16fbddac0
Polaris <108342258+Polariswright@users.noreply.github.com>
2022-08-09 22:45:50 -0300

    v0.1
    yayayayaaa

---
+++ script.js
@@ -0,0 +1,16 @@
+document.write('Hello World!')
+let x = 10;
+if(x){
+    x = 5, document.write('Your number is lower than 10.')
+}
+else {
+    document.write('Your number is bigger or equal 10.')
+};
+
+let y = 8;
+if(x>y){
+    document.write(x)
+}
+else{
+    document.write(y)
+};
